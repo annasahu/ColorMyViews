1e0447544a200c64efb174a06bb478c1488610c6
Anna Sahu <73133104+annasahu@users.noreply.github.com>
2020-10-31 10:55:42 -0300

    using shared preferences

--- app/src/main/java/com/wcc/colormyviews/MainActivity.kt
+++ app/src/main/java/com/wcc/colormyviews/MainActivity.kt
@@ -1,28 +1,40 @@
 package com.wcc.colormyviews
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var currentColor = R.color.grey
-    //var boxOneColor =
+    var boxOneColor = R.color.grey
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sharedPreferences = this.getSharedPreferences("colors", Context.MODE_PRIVATE)
+        val color = sharedPreferences.getInt("color_box_one", R.color.grey)
+        box_one_text.setBackgroundResource(color)
+
         setColorButtonAction()
         setBoxColor()
     }
 
-    override fun onDestroy(){
-        super.onDestroy()
+    override fun onStop() {
+        super.onStop()
+
+        val sharedPreferences = this.getSharedPreferences("colors", Context.MODE_PRIVATE)
+        val sharedPreferencesEditor = sharedPreferences.edit()
+
+        sharedPreferencesEditor.putInt("color_box_one", boxOneColor)
+        sharedPreferencesEditor.apply()
     }
 
     private fun setBoxColor() {
         box_one_text.setOnClickListener {
             it.setBackgroundResource(currentColor)
+            boxOneColor = currentColor
         }
         box_two_text.setOnClickListener {
             it.setBackgroundResource(currentColor)
